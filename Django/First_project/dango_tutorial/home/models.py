b2cf04302885dad4f074363edeaa85ce01a23a31
sanin6969 <145665810+sanin6969@users.noreply.github.com>
2024-04-02 16:12:32 +0530

    'hospitak'

--- Django/First_project/dango_tutorial/home/models.py
+++ Django/First_project/dango_tutorial/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Departments(models.Model):
+    dep_name=models.CharField(max_length=100)
+    dep_description=models.TextField()
+class Doctors(models.Model):
+    doc_name=models.CharField(max_length=250)
+    doc_spec=models.CharField(max_length=250)
+    dep_name=models.ForeignKey(Departments,on_delete=models.CASCADE)
+    doc_image=models.ImageField( upload_to='doctors')
+    
--- Django/First_project/dango_tutorial/home/views.py
+++ Django/First_project/dango_tutorial/home/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from .models import Departments,Doctors
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def booking(request):
+    return render(request,'booking.html')
+
+def doctors(request):
+    Dict_doc={
+        'doc':Doctors.objects.all()
+    }
+    return render(request,'doctors.html')
+
+def Contact(request):
+    return render(request,'contact.html')
+
+def department(request):
+    Dict_dep={
+        'dept':Departments.objects.all()
+    }
+    return render(request,'department.html',Dict_dep)
